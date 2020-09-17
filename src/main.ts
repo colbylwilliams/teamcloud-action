57768626e2203de168bfe7360e0f32a1ff404e66
Colby Williams <ColbyLWilliams@gmail.com>
2020-09-17 16:40:57 -0400

    initial try

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,49 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { create, UploadOptions } from '@actions/artifact'
+import { dirname } from 'path'
+import { promises as fs } from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const workspace = process.env.GITHUB_WORKSPACE
+    const eventPath = process.env.REACT_APP_TC_API_URL
+    const commandPath = `${workspace}/command.json`
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (!eventPath) {
+      core.setFailed('Unable to find event payload path. Environment variable GITHUB_WORKSPACE is undefined.')
+    } else {
+      const buffer = await fs.readFile(eventPath)
+      const event = JSON.parse(buffer.toString())
+      const command = event.client_payload
 
-    core.setOutput('time', new Date().toTimeString())
+      if (!command) {
+        core.setFailed('Unable get command from event payload.')
+      } else {
+        await fs.writeFile(commandPath, JSON.stringify(command))
+
+        const artifactClient = create()
+        const options: UploadOptions = {
+          continueOnError: false
+        }
+
+        const uploadResponse = await artifactClient.uploadArtifact(
+          'command',
+          [commandPath],
+          dirname(commandPath),
+          options
+        )
+
+        if (uploadResponse.failedItems.length > 0) {
+          core.setFailed(
+            `An error was encountered when uploading ${uploadResponse.artifactName}. There were ${uploadResponse.failedItems.length} items that failed to upload.`
+          )
+        } else {
+          core.info(`Artifact ${uploadResponse.artifactName} has been successfully uploaded!`)
+        }
+
+        core.setOutput('command', command)
+      }
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
